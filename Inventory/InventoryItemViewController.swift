6cb38d6e366168d4c6730fe4ef365ce192e99eae
Jenifer Brown <Jennyvbrown@gmail.com>
2018-10-26 17:04:31 -0600

    Set up complete.

---
+++ Inventory/InventoryItemViewController.swift
@@ -0,0 +1,30 @@
+//
+//  InventoryItemViewController.swift
+//  Inventory
+//
+//  Created by Jenifer Brown on 10/26/18.
+//  Copyright © 2018 Jenifer Brown. All rights reserved.
+//
+
+import UIKit
+
+class InventoryItemViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
